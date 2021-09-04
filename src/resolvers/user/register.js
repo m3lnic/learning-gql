c445dcff100c4d0c4bd5d76d68ffbcb8ffd92575
Melody Nicholls <melody.astrid.nicholls@gmail.com>
2021-09-04 19:27:59 +0100

    Add email validation

--- src/resolvers/user/register.js
+++ src/resolvers/user/register.js
@@ -1,6 +1,9 @@
+const { ValidationError } = require("apollo-server-errors");
 const { validateEmail } = require("../../helpers");
 
 module.exports = (_, { login, email, password}) => {
-  console.log(login, password);
+  if (!validateEmail(email)) {
+    throw new ValidationError('The email provided is invalid');
+  }
   return 'pancake';
 }
